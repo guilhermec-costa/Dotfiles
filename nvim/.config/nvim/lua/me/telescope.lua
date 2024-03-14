6513f79763b71deadf41df9cca4bb9a9c93cc807
Guilherme China <echina725@gmail.com>
2024-03-13 21:13:32 -0300

    add some picker configuration

--- nvim/.config/nvim/lua/me/telescope.lua
+++ nvim/.config/nvim/lua/me/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
 
 
-vim.keymap.set('n', '<leader>ff', function() builtin.find_files({hidden=true}) end, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 --[[ vim.keymap.set('n', '<leader>fH', function() builtin.find_files({hiddeo=true}) end, {}) ]]
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, {})
@@ -18,18 +18,32 @@ vim.keymap.set('n', '<leader>D', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>B', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>C', builtin.git_commits, {})
 vim.keymap.set('n', '<leader>cb', builtin.git_bcommits, {})
+vim.keymap.set('n', '<leader>S', builtin.git_stash, {})
 vim.keymap.set('n', '<leader>qf', builtin.quickfix, {})
 vim.keymap.set('n', '<leader>rs', builtin.search_history, {})
 vim.keymap.set('n', '<leader>P', builtin.pickers, {})
 
 
 -- enable colorscheme preview on switch
+
+local actions = require('telescope.actions')
 require("telescope").setup {
     pickers = {
         colorscheme = {
             enable_preview = true
+        },
+
+        -- enable live grep to work on hidden files
+        live_grep = {
+            additional_args = function()
+                return { "--hidden" }
+            end
+        },
+
+        find_files = {
+            hidden = true
         }
-    }
+    },
 }
 
 vim.keymap.set('n', '<leader>/', function()
